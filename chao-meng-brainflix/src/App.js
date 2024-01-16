7f1031c7bd65493a522b27e3bb9f6527ce4883ba
Chao Meng <cmeng036@gmail.com>
2024-01-16 12:49:07 -0500

    added readme and header

--- chao-meng-brainflix/src/App.js
+++ chao-meng-brainflix/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- chao-meng-brainflix/src/components/Header/Header.js
+++ chao-meng-brainflix/src/components/Header/Header.js
@@ -1,3 +1,23 @@
+import logo from "../../assets/images/BrainFlix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import "./Header.scss";
 function Header(){
-return
+return<header>
+    <div className="header">
+        <a className="header__title"> 
+            <img className="header__logo" src={logo}></img>
+        </a>
+        <div className="header__container">
+            <div className="header__container--left">
+                <input className="header__input" placeholder="Search" type="text"/>   
+                <button className="header__button">UPLOAD</button>
+            </div>
+        <div className="header__container--right">
+            <img className="header__avatar" src={avatar}></img>
+        </div>
+         </div>
+    </div>
+</header>
+
 }
+export default Header;
